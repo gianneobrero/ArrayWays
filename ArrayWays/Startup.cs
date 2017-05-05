e6cbaf875ccc7c701c88dc8f2b1dda6fa0461c71
giann <giann@WILLSMITH>
2017-05-05 15:01:45 -0500

    Add project files.

---
+++ ArrayWays/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArrayWays.Startup))]
+namespace ArrayWays
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
